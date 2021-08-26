9622a822deaf901ee1d57dd759e0dca5eec5956b
Lior Lahav <LahavLior@gmail.com>
2021-08-26 10:00:13 +0300

    Added: support for high DPI monitors

--- NetSettings/View/DataViewPlacement.cs
+++ NetSettings/View/DataViewPlacement.cs
@@ -8,13 +8,13 @@ namespace NetSettings
 {
     internal class DataViewPlacement
     {
-        public int LineSpacing = 25;
+        public int LineSpacing = 30;
         public int TitleMaxWidth = 150;
         public int TitleSpacing = 30;
         public int ControlMaxWidth = 80;
         public int ControlSpacing = 20;
-        public int LineHeight = 20;
-        public int DefaultButtonWidth = 50;
+        public int LineHeight = 30;
+        public int DefaultButtonWidth = 70;
         public int HorizontalMArgin = 20;
     }
 }
